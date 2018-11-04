0046378284bdf47e2930c80d37a19b0cc4a908c6
Bak David <bak.david96@gmail.com>
2018-11-05 01:34:21 +0200

    Made registration and login functionality, connected to the firebase database and ran a test with it successfully

---
+++ app/src/main/java/com/example/pedrohuan/sapinewsandroidapp/listnews/ListNewsActivity.java
@@ -0,0 +1,15 @@
+package com.example.pedrohuan.sapinewsandroidapp.listnews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pedrohuan.sapinewsandroidapp.R;
+
+public class ListNewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_news);
+    }
+}
